9e23c858a02483374e12b865c48264b448d42683
namihiran <namihiran@gmail.com>
2015-12-23 22:05:02 +0900

    apply gradle and thymeleaf

---
+++ pikbook/src/main/java/jp/co/namihira/pikbook/web/controller/view/AbstractViewController.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright 2015 Kosuke Namihira All Rights Reserved.
+ */
+package jp.co.namihira.pikbook.web.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * View関連のコントローラの抽象クラス
+ */
+@Controller
+@RequestMapping(value = "/view")
+public abstract class AbstractViewController {
+
+}
